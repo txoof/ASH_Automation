119f9fc580ab88245ca1b8cd82bf5c3c7958a165
jfkalkman <117269426+jfkalkman@users.noreply.github.com>
2022-11-02 13:04:47 +0100

    Create parse_ps_reports.sh
    Update parse_ps_reports.sh
    
    update docs
    
    Update parse_ps_reports.sh
    
    Update parse_ps_reports.sh
    
    Update parse_ps_reports.sh
    
    Update parse_ps_reports.sh
    
    formatting
    
    Update parse_ps_reports.sh
    
    add ignore
    
    add variable for group suffix
    
    fix mktemp path
    
    fix bugs with email body
    
    archive processed reports

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ PowerSchool SIS generates reports on a schedule set in the Data Export Manager (
 
 Documents deposited on the SSH host are processed and forwarded on to the appropriate user groups with links to documentation and instructions particular to the document. All documents should only be mailed to google group addresses within the domain. Distribution lists are maintained exclusively through the google groups.
 
+
+## Report setup
+
+Report names need to be formatted as `destination_group-_-report_name.csv`. The `destination_group` must match a google group in the form of `destination_group_sis_reports@ash.nl`. The delimiter between the destination group and the report name must always be `-_-`. 
+## Email setup
+
+All outgoing emails will get `sis_textbody/default_body.txt` appended to the email.
+
+Specific text will be added to outgoing email. 
+
 ## TO DO
 
 - [ ] Set up sismailer user on SSH host
@@ -17,5 +27,5 @@ Documents deposited on the SSH host are processed and forwarded on to the approp
 - [ ] Setup cron job for sis mailer
 - [ ] Write standard, individual text for mailer
 - [ ] Set up Google groups
-- [ ] 
+- [ ] more robust parsing of the incoming report name delimiter using a regex
 
---
+++ parse_ps_reports/parse_ps_reports.sh
@@ -0,0 +1,212 @@
+#!/bin/zsh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
+host=$( hostname )
+setopt null_glob
+autoload is-at-least
+
+# Determine directory where this script is running from
+parent_dir=$( /usr/bin/dirname "$0" )
+
+SCRIPT_PATH="${0}"
+SCRIPT=$( basename "${SCRIPT_PATH}" )
+
+
+today_marker=$( date +%C%y%m%d )
+year_marker=$( date +%C%y )
+
+#
+# ------------------------------------------------------------------------------------------------------------------------
+#
+# Jasper Kalkman & Aaron Ciuffo
+#
+# First attempt to create a Powerschool batch report mailer to google groups
+#
+# ------------------------------------------------------------------------------------------------------------------------
+#
+## configuration
+
+# the `email_group_suffix` will be used whenever forwarding reports 
+# see `Email Setup` in README.md
+email_group_suffix="_sis_reports@ash.nl"
+
+
+current_user="sismailer"
+#home=$( eval echo ~$current_user )
+home=$HOME
+
+
+# Default logging locations
+#
+logfolder="${home}/sis_log"
+logfile="${logfolder}/${SCRIPT}.$today_marker.log"
+
+# Default sftp location for Powerschool
+#
+sftp_location="${home}/sis_incoming"
+
+# Default attachement
+#
+body_text_location="${home}/sis_textbody"
+
+# Default text body allways used
+#
+default_body_text="${body_text_location}/default_body.txt"
+
+report_archive="$home/old_reports"
+
+
+
+#
+# ------------------------------------------------------------------------------------------------------------------------
+#
+
+
+# Echo function
+message_Log () 
+{
+	message="${@}"
+    
+    # Date and Time function for the log file
+    fDateTime () { echo $(date +"%a %b %d %T"); }
+
+    # Title for beginning of line in log file
+    Title="SIS Reports"
+
+    # Header string function
+    fHeader () { echo $(fDateTime) - $Title; }
+ 	echo $(fHeader) "$message" >> "$logfile"
+ 	echo $(fHeader) "$message" 
+}
+
+
+# ----------------------------------------------------------------------------------------------
+#
+function Create_Dummy()
+{
+	for csv_file in "${CSV_Check_list[@]}"; do
+		
+		ARRAY=("${(@s/:/)csv_file}")
+		Test_file="$ARRAY[1]"
+		email="$ARRAY[2]"
+		message_Log "${test_msg} > ${test_location}/${Test_file}--Attachment_${Test_file}.csv"
+		echo "${test_msg}" > "${test_location}/${Test_file}--Attachment_${Test_file}.csv"
+	done
+}
+
+
+#
+#
+# ----------------------------------------------------------------------------------------------
+#
+function parse_ps_reports()
+{	
+	mk_tmp=$(which mktemp)
+	#tmp_location=$( $mk_tmp -d $home/parse_ps_reports.XXXX )
+	tmp_location=$( $mk_tmp -d )
+	
+	message_Log "Create temp directory: $tmp_location"
+	
+	array_csv=( ${sftp_location}/*.csv )
+	
+	for csv_file in $array_csv
+	do
+		name=$( basename $csv_file .csv )
+		base_address=$( echo "${name}" | cut -d "-" -f 1 )
+		
+		#
+		# is formatted ....
+		#
+		email_address="${base_address}$email_group_suffix"
+		
+		#
+		File_name=$( echo "${name}" | awk -F"-_-" '{print $2}' )
+		
+		
+		# 
+		email_Body="Body_${File_name}.txt"
+		Attachment="Attachment_${File_name}.csv"
+		# append this text to every outgoing email that matches the base address
+		Info_Body="${base_address}.txt"
+		
+		message_Log "Create the email body"
+
+		if [[ -e "${default_body_text}" ]]; 
+		then
+			message_Log "Append the default text to this message"
+			
+			echo " " >>"${tmp_location}/${email_Body}"
+			
+			cat "${default_body_text}" >>"${tmp_location}/${email_Body}"
+			
+			echo " " >>"${tmp_location}/${email_Body}"
+		fi
+
+		if [[ -e "${body_text_location}/${Info_Body}" ]]; 
+		then
+			message_Log "Append the info text to this message"
+			message_Log "$Info_Body"
+			
+			echo " " >>"${tmp_location}/${email_Body}"
+			
+			cat "${body_text_location}/${Info_Body}" >>"${tmp_location}/${email_Body}"
+			
+			echo " " >>"${tmp_location}/${email_Body}"
+		else
+			message_Log "No additional Info_body found"
+
+			echo " " >>"${tmp_location}/${email_Body}"
+			
+			echo "Please check the attached report ${File_name}" >>"${tmp_location}/${email_Body}"
+			
+			echo " " >>"${tmp_location}/${email_Body}"
+		fi
+		
+		message_Log "Create the attachment ${Attachment}"
+		uuencode "${csv_file}"  "${tmp_location}/Attachment_${File_name}.csv" >>"${tmp_location}/${Attachment}"
+		
+		message_Log "Append the attachment to this message"
+		cat "${tmp_location}/${Attachment}" >>"${tmp_location}/${email_Body}"
+		
+		#email_address="jkalkman@ash.nl"
+		#email_address="aciuffo@ash.nl"
+		
+		message_Log "Send message to ${email_address}"
+		message_Log "${tmp_location}/${email_Body} | mail -s ${name} ${email_address}"
+		
+		cat "${tmp_location}/${email_Body}" | mail -s "${name}" "${email_address}"
+
+		# archive reports
+		message_Log "Archiving report $csv_file in $report_archive"
+
+		mv $csv_file $report_archive 
+	done
+	
+	message_Log "Cleaning up temporary files" 
+	rm -rfd "${tmp_location}"
+}
+
+
+#
+# ----------------------------------------------------------------------------------------------
+#
+
+message_Log " "
+message_Log "Make SIS support folders"
+
+mkdir -p "${logfolder}"
+mkdir -p "${sftp_location}"
+mkdir -p "${body_text_location}"
+
+message_Log " "
+
+message_Log "parse_ps_reports"
+
+parse_ps_reports
+
+message_Log " "
+
+#
+# ----------------------------------------------------------------------------------------------
+#
+
+exit 0
